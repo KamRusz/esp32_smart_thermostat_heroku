21b44006a23c9b53fab0e683d1f54a09d41a3ea8
Kamil <kamilrusztyn@gmail.com>
2022-03-18 15:38:09 +0100

    initial commit

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=thermostat.py
+FLASK_ENV=development
+FLASK_DEBUG=True
---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from flask import render_template, request
+import datetime
+from app import app
+import json
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Kamil'}
+    return render_template('index.html', title='Home', user=user, utc_dt=datetime.datetime.now().strftime("%H:%M:%S"))
+
+slownik = {"target_temp":"25", "user_override":"1"}
+
+
+@app.route('/hello', methods=['GET','POST'])
+def welcome():   
+    payload = json.loads(request.data)
+    print("api =",payload["api_key"])
+    print("room_temp =",payload["room_temp"])
+    print("room_humi =",payload["room_humi"])
+    return slownik
+    #print(request.args["temp"],request.args["humi"])
+    #if api_key == "123456789":
+    #    return slownik
+    #else:
+    #    return "zonk"
+
+@app.route('/hello2', methods=['GET','POST'])
+def welcome2():   
+    payload = json.loads(request.data)
+    print("user_temp =",payload["user_temp"])
+    return "user temp ovveride"
